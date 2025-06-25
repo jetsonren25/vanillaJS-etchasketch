51a2cd7f0acc133b99c21070eb98f723c9718425
Javone <javosmith@indigoempirellc.com>
2025-06-25 11:14:47 -0400

    edited the createBlock func and added styling to get flex items 16 per row

--- app.js
+++ app.js
@@ -5,10 +5,10 @@ container.setAttribute('id','main-cont')
 
 
 function createBlock(){
-    for(let x = 16; x > 0; x--){
+    for(let x = (16*16); x > 0; x--){
         let block = document.createElement('div')
         block.setAttribute('class','block')
-        block.textContent = 'block'
+        // block.textContent = 'block'
         container.appendChild(block)
     }
 }
