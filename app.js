f1310fe2e3a5e2ea01b2a3285a61bb6490ab0d7f
Javone <javosmith@indigoempirellc.com>
2025-06-25 13:48:51 -0400

    added event listener to btn and set prompt for user

--- app.js
+++ app.js
@@ -2,9 +2,18 @@ let alphanum = 'abcdef0123456789'
 
 let body = document.querySelector('body')
 
+let btn = document.createElement('button')
+btn.textContent = 'New Game'
+btn.setAttribute('id','btn')
+
 let container = document.createElement('div')
 container.setAttribute('id','main-cont')
 
+
+
+btn.addEventListener('click', () => {
+    let input = prompt('How many squares per side?')
+})
 
 function createBlock(){
     for(let x = (16*16); x > 0; x--){
@@ -12,7 +21,7 @@ function createBlock(){
         block.setAttribute('class','block')
         block.addEventListener('mouseover', (e) => e.target.style.backgroundColor = `${randomColor()}`)
         block.addEventListener('mouseleave', (e) => {
-            e.target.style.transition = 'background-color 0.5s ease-in 0.8s'
+            e.target.style.transition = 'background-color 0.8s ease-in 1s'
             e.target.style.backgroundColor = ''
         })
         container.appendChild(block)
@@ -28,5 +37,6 @@ const randomColor = () => {
 }
 
 
+body.appendChild(btn)
 body.append(container)
 createBlock()
